a2046d8c55a95205a9bf2cfa9dbc8e94898bb045
Tamuna-Z <tamunazurabashvili3@gmail.com>
2024-06-03 22:22:12 +0400

    accordion structure

---
+++ script.js
@@ -0,0 +1,137 @@
+// slider data/photos
+
+// let data = [
+//     {
+//         id: 1,
+//         imgAddress:'images/usaid.webp',
+
+//     },
+//     {
+//         id: 2,
+//         imgAddress:'images/space.webp',
+
+//     },
+//     {
+//         id: 3,
+//         imgAddress:'images/tineti.webp',
+
+//     },
+
+    
+// ];
+
+let data=[
+    {
+        id: 1,
+        imageUrl: 'https://imgur.com/VFLEhMj',
+    },
+    {
+        id: 2,
+        imageUrl: 'https://thumbs.dreamstime.com/z/traditional-georgian-sweets-called-churchkhela-churchela-candle-shaped-grape-juice-candy-nuts-inside-traditional-georgian-161801425.jpg',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0lwHzvnxdwvb7S9D4wHAlqDBJoxRRQZ5re5Et0xmLnExan2nhtXTn9CUxJzF9Wp_RtG0&usqp=CAU'
+    },
+      
+];
+
+
+
+
+let arrowLeft = document.getElementById ('arrow-left');
+let arrowRight = document.getElementById ('arrow-right');
+let sliderContainer = document.getElementById ('slider');
+let dotsList = document.getElementsByClassName ('dot');
+
+let sliderIndex = 0;
+
+function createATag (item) {
+    let aTag = document.createElement('a');
+    aTag.setAttribute('href', item.url);
+    aTag.classList.add('slider-a');
+
+    return aTag;
+}
+
+function createImgTag(item) {
+    sliderContainer.style.backgroundImage = 'url('+ item.imageUrl +')';
+    sliderContainer.style.backgroundRepeat = "no-repeat";
+    sliderContainer.style.backgroundSize = "cover";
+}
+
+
+function createH2Tag (item) {
+    let h2Tag = document.createElement('a');
+    h2Tag.setAttribute.href = item.url;
+    h2Tag.classList.add('slider-title');
+    h2Tag.append(item.title);
+    return h2Tag;
+}
+
+function createDots() {
+    let dots = document.createElement('div');
+    dots.classList.add('dots');
+
+    data.forEach((element) => {
+        let dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.setAttribute('data-id', element.id-1);
+
+        dot.onclick = () => {
+            let id = event.target.getAttribute('data-id');
+            sliderIndex = id;
+            setSlider();
+        }
+        dots.appendChild(dot);
+    })
+    return dots;
+}
+
+function setSlider() {
+    sliderContainer.innerText = '';
+    createImgTag(data[sliderIndex]);
+    let sliderItem = createATag (data[sliderIndex]);
+    let title = createH2Tag (data[sliderIndex]);
+    let dots = createDots();
+    sliderItem.appendChild(title);
+    sliderContainer.appendChild(sliderItem);
+    sliderContainer.appendChild(dots);
+    currentDotActive();
+    
+}
+
+function currentDotActive () {
+    dotsList[sliderIndex].classList.add('active')
+}
+
+function arrowLeftClick () {
+    if (sliderIndex == 0) {
+        sliderIndex = data.length;
+     }
+    sliderIndex--;
+    setSlider();
+}
+
+function arrowRightClick () {   
+    if(sliderIndex == data.length-1) {
+        sliderIndex = -1;
+    }
+    sliderIndex++;
+    setSlider();
+}
+//1.
+arrowLeft.addEventListener('click', arrowLeftClick)
+arrowRight.addEventListener('click', arrowRightClick)
+//2.
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+        arrowLeftClick();
+    } else if (event.keyCode == 39) {
+        arrowRightClick();
+    }
+})
+//3.
+setInterval( () => {
+    arrowRightClick ();
+}, 4000);
+
+setSlider();
+
